9c1e8c17ef7c80ff5547436ce5c16f956f40bbb0
Le Linh <catbuituyetvoi@gmail.com>
2015-04-26 11:19:42 +0700

    add postgre to gem

--- db/migrate/20150423110314_create_posts.rb
+++ db/migrate/20150423110314_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.integer :object_id
-      t.string :type
+      t.string :post_type
 
       t.timestamps null: false
     end
